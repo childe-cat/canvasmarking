8169e9fde351dab53e38d6b647d3944d986c5341
雷 李 <l-chats@qq.com>
2024-08-30 15:33:47 +0800

    2024-08-30 / second 完善图片加载方式 添加部分函数

--- CanvasMarking.js
+++ CanvasMarking.js
@@ -1,85 +1,25 @@
-/**
- * 画笔类
- * @param imageUrl 图片地址
- * @param markers 标注列表
- * @param markerType 标注类型
- * @param markerColor 标注颜色
- * @param markerRadius 标注半径
- * @param markerLineWidth 标注线宽
- * @param hasText 是否有文字
- * @param textDirection 文字方向
- * @param autoScaleMarker 是否自动缩放标注
- * @param clickMethod 点击额外事件，给标注添加额外属性
- * @param imageLoadWay 图片加载方式，默认为none
- */
-interface Options {
-    imageUrl: string;
-    markers: Array<marker>;
-    markerType: string;
-    markerColor: string
-    markerRadius: number;
-    markerLineWidth: number;
-    hasText: boolean;
-    textDirection: string;
-    autoScaleMarker: boolean;
-    imageLoadWay: string;
-    clickMethod: Function;
-}
-
-interface marker {
-    x: number;
-    y: number;
-    uuid: string;
-    name: string | null;
-    markerType: string;
-    markerColor: string;
-    markerRadius: number;
-    markerLineWidth: number;
-    options: any
-}
-
+"use strict";
 class CanvasMarking {
-    private imageUrl: string;
-
-    private scale: number = 1;
-    private lastX: number = 0;
-    private lastY: number = 0;
-    private offsetX: number = 0;
-    private offsetY: number = 0;
-
-    private clickFlag: boolean = true;
-    private draggingFlag: boolean = false;
-
-    private canvas: HTMLCanvasElement;
-    private ctx: CanvasRenderingContext2D;
-    private hasText: boolean;
-    private textDirection: string;
-    private markers: Array<marker>;
-    private markerType: string;
-    private markerColor: string;
-    private markerRadius: number;
-    private markerLineWidth: number;
-    private autoScaleMarker: boolean;
-    private image!: HTMLImageElement;
-    private drawWidth: number;
-    private drawHeight: number;
-    private imageLoadWay: string;
-
-    private animationFrameId: number | null = null;
-    private clickMethod: Function;
-
     /**
      * 画笔类
      * @param canvasId canvas的id
      * @param options 配置项，可选项包括图片地址、画笔类型、画笔颜色
      */
-    constructor(canvasId: string, options: Options) {
-        this.canvas = document.getElementById(canvasId) as HTMLCanvasElement;
-        this.ctx = this.canvas.getContext('2d')!;
+    constructor(canvasId, options) {
+        this.scale = 1;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.clickFlag = true;
+        this.draggingFlag = false;
+        this.animationFrameId = null;
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext('2d');
         if (!this.canvas) {
             throw new Error('canvasId错误,未找到canvas元素');
         }
-        if(!options.markers){
+        if (!options.markers) {
             throw new Error('markers不能为空');
         }
         this.imageUrl = options.imageUrl;
@@ -92,91 +32,83 @@ class CanvasMarking {
         this.textDirection = options.textDirection || 'right';
         this.autoScaleMarker = options.autoScaleMarker || false;
         this.imageLoadWay = options.imageLoadWay || 'none';
-        this.clickMethod = options.clickMethod
+        this.clickMethod = options.clickMethod;
         this.drawWidth = 0;
         this.drawHeight = 0;
         this.loadImage();
     }
-
     /**
      * 注册事件
      */
     initEvent() {
-        this.canvas.addEventListener('click', this.handleClick.bind(this))
-        this.canvas.addEventListener('wheel', this.handleWheel.bind(this))
-        this.canvas.addEventListener('mousedown', this.handleMouseDown.bind(this))
-        this.canvas.addEventListener('mousemove', this.handleMouseMove.bind(this))
-        this.canvas.addEventListener('mouseup', this.handleMouseUp.bind(this))
-        this.canvas.addEventListener('mouseout', this.handleMouseOut.bind(this))
+        this.canvas.addEventListener('click', this.handleClick.bind(this));
+        this.canvas.addEventListener('wheel', this.handleWheel.bind(this));
+        this.canvas.addEventListener('mousedown', this.handleMouseDown.bind(this));
+        this.canvas.addEventListener('mousemove', this.handleMouseMove.bind(this));
+        this.canvas.addEventListener('mouseup', this.handleMouseUp.bind(this));
+        this.canvas.addEventListener('mouseout', this.handleMouseOut.bind(this));
     }
-
     /**
      * 加载图片
      */
-    loadImage(){
-        this.image = new Image() as HTMLImageElement;
+    loadImage() {
+        this.image = new Image();
         this.image.onload = () => {
             if (this.imageLoadWay === 'none') {
                 // 获取 canvas 的显示大小
                 const rect = this.canvas.getBoundingClientRect();
                 const canvasWidth = rect.width;
                 const canvasHeight = rect.height;
-
                 // 将 canvas 的 width 和 height 设置为与 CSS 定义的大小一致
                 this.canvas.width = canvasWidth;
                 this.canvas.height = canvasHeight;
-
                 // 获取图片的原始宽高
                 const imgWidth = this.image.width;
                 const imgHeight = this.image.height;
-
                 // 计算图片的缩放比例
                 const scaleX = canvasWidth / imgWidth;
                 const scaleY = canvasHeight / imgHeight;
                 const scale = Math.min(scaleX, scaleY);
-
                 // 根据缩放比例设置图片在 canvas 中的宽高
                 this.drawWidth = imgWidth * scale;
                 this.drawHeight = imgHeight * scale;
-
                 // 计算偏移量，使图片居中显示
                 this.offsetX = (canvasWidth - this.drawWidth) / 2;
                 this.offsetY = (canvasHeight - this.drawHeight) / 2;
-            }else if(this.imageLoadWay === 'fill'){
+            }
+            else if (this.imageLoadWay === 'fill') {
                 this.canvas.width = this.image.width;
                 this.canvas.height = this.image.height;
                 this.drawWidth = this.canvas.width;
                 this.drawHeight = this.canvas.height;
             }
-
             this.drawImage();
             this.initEvent();
-        }
+        };
         this.image.src = this.imageUrl + '?p=' + Math.random().toString().replace('.', '');
     }
-
     /**
      * 绘制图片
      */
-    drawImage(): void {
-        this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    drawImage() {
+        var _a, _b;
+        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
         const width = this.drawWidth * this.scale;
         const height = this.drawHeight * this.scale;
-        console.log(this.offsetX, this.offsetY, width, height)
-        this.ctx?.drawImage(this.image, this.offsetX, this.offsetY, width, height);
+        console.log(this.offsetX, this.offsetY, width, height);
+        (_b = this.ctx) === null || _b === void 0 ? void 0 : _b.drawImage(this.image, this.offsetX, this.offsetY, width, height);
         this.markers.forEach((marker) => {
-            this.drawMarker(marker)
+            this.drawMarker(marker);
         });
     }
-
     /**
      * 绘制标注
      * @param marker 标注对象
      */
-    drawMarker(marker: marker) {
+    drawMarker(marker) {
         this.ctx.lineWidth = marker.markerLineWidth;
         this.ctx.strokeStyle = marker.markerColor;
-        this.ctx.beginPath()
+        this.ctx.beginPath();
         const x = marker.x * this.scale + this.offsetX;
         const y = marker.y * this.scale + this.offsetY;
         const r = this.autoScaleMarker ? marker.markerRadius * this.scale : marker.markerRadius;
@@ -191,7 +123,7 @@ class CanvasMarking {
                 this.ctx.lineTo(x + r, y + r);
                 break;
             case 'square':
-                this.ctx.rect(x - r, y - r, r * 2, r * 2)
+                this.ctx.rect(x - r, y - r, r * 2, r * 2);
                 break;
             default:
                 this.ctx.arc(x, y, r, 0, Math.PI * 2);
@@ -200,7 +132,7 @@ class CanvasMarking {
         this.ctx.stroke();
         if (this.hasText) {
             this.ctx.font = '12px Georgia';
-            const markerName = ''
+            const markerName = '';
             this.ctx.fillText(marker.name || markerName, x + r + 6, y + r);
             //文字方向
             switch (this.textDirection) {
@@ -211,29 +143,28 @@ class CanvasMarking {
                     this.ctx.fillText(marker.name || markerName, x - r - 6 - this.ctx.measureText(marker.name || '').width, y + r);
                     break;
                 case 'top':
-                    this.ctx.fillText(marker.name || markerName, x + r + 6, y - r)
+                    this.ctx.fillText(marker.name || markerName, x + r + 6, y - r);
                     break;
                 case 'bottom':
-                    this.ctx.fillText(marker.name || markerName, x + r + 6, y + r + 12)
+                    this.ctx.fillText(marker.name || markerName, x + r + 6, y + r + 12);
                     break;
                 default:
                     this.ctx.fillText(marker.name || markerName, x + r + 6, y + r);
             }
         }
     }
-
     /**
      * 鼠标点击监听事件，新增一个标注
      * @param e
      */
-    handleClick(e: MouseEvent) {
+    handleClick(e) {
         if (e.target === this.canvas && this.clickFlag) {
             const rect = this.canvas.getBoundingClientRect();
             const scaleX = this.canvas.width / rect.width;
             const scaleY = this.canvas.height / rect.height;
             const x = ((e.clientX - rect.left) * scaleX - this.offsetX) / this.scale;
             const y = ((e.clientY - rect.top) * scaleY - this.offsetY) / this.scale;
-            const newMarker: marker = {
+            const newMarker = {
                 x,
                 y,
                 uuid: '',
@@ -249,43 +180,40 @@ class CanvasMarking {
             this.drawImage();
         }
     }
-
     /**
      * 鼠标滚轮监听事件，缩放canvas图像
      */
-    handleWheel(e: WheelEvent) {
+    handleWheel(e) {
         if (e.target === this.canvas) {
-            e.preventDefault()
+            e.preventDefault();
             const rect = this.canvas.getBoundingClientRect();
             const mouseX = (e.clientX - rect.left) * (this.canvas.width / rect.width);
             const mouseY = (e.clientY - rect.top) * (this.canvas.height / rect.height);
             const preScale = this.scale;
             if (e.deltaY < 0) {
                 this.scale *= 1.1;
-            } else {
+            }
+            else {
                 this.scale /= 1.1;
             }
             const scaleChange = this.scale / preScale;
             this.offsetX -= (mouseX - this.offsetX) * (scaleChange - 1);
             this.offsetY -= (mouseY - this.offsetY) * (scaleChange - 1);
-
-            this.drawImage()
+            this.drawImage();
         }
     }
-
     /**
      * 鼠标按下监听事件，获取按下鼠标位置
      */
-    handleMouseDown(e: MouseEvent) {
+    handleMouseDown(e) {
         this.draggingFlag = true;
         this.lastX = e.clientX;
         this.lastY = e.clientY;
     }
-
     /**
      * 鼠标移动监听事件，拖动canvas图像
      */
-    handleMouseMove(e: MouseEvent) {
+    handleMouseMove(e) {
         if (this.draggingFlag) {
             this.clickFlag = false;
             const x = e.clientX - this.lastX;
@@ -297,7 +225,6 @@ class CanvasMarking {
             if (this.animationFrameId !== null) {
                 cancelAnimationFrame(this.animationFrameId);
             }
-
             // 使用requestAnimationFrame来绘制图像
             this.animationFrameId = requestAnimationFrame(() => {
                 this.drawImage();
@@ -305,7 +232,6 @@ class CanvasMarking {
             });
         }
     }
-
     /**
      * 鼠标抬起监听事件，允许点击
      */
@@ -313,39 +239,34 @@ class CanvasMarking {
         this.draggingFlag = false;
         setTimeout(() => {
             this.clickFlag = true;
-        }, 1)
+        }, 1);
     }
-
     /**
      * 鼠标移出监听事件，停止拖拽
      */
     handleMouseOut() {
         this.draggingFlag = false;
     }
-
     /**
      * 删除所有标注
      */
-    deleteAll(){
-        this.markers = []
-        this.drawImage()
+    deleteAll() {
+        this.markers = [];
+        this.drawImage();
     }
-
     /**
      * 删除指定标注
      */
-    deleteMarker({ name, value }: { name?: string, value: string }) {
+    deleteMarker({ name, value }) {
         this.markers = this.markers.filter(marker => {
             return name ? marker.options[name] !== value : marker.uuid !== value;
         });
-        this.drawImage()
+        this.drawImage();
     }
-
     /**
      * 获取所有标注
      */
-    getMarkers(){
-        return this.markers
+    getMarkers() {
+        return this.markers;
     }
 }
-
--- README.md
+++ README.md
@@ -1,10 +1,98 @@
 # CanvasMarking
-canvas轻量图片标注功能
-### 1.引入CanvasMarking.ts文件
-    <script src="CanvasMarking.ts"></script>
+### canvas轻量图片标注功能
+## 1.引入CanvasMarking.js文件
+```js
+<script src="CanvasMarking.js"></script>
+```
 
-### 2.调用方法
-    const canvas = new CanvasMarking('canvasId','imageUrl');
+## 2.调用方法
+```js
+const canvas = new CanvasMarking('canvasId',options);
+```
 
-### 3.内置函数
+## 3.内置函数
+```js
+/**
+ * 获取所有标注点信息
+ */
+canvas.getMarkers();
+
+/**
+ * 删除所有标记信息
+ */
+canvas.deleteAll();
+
+/**
+ * param {string} value 值
+ * param {string} name? 标记点名称,默认为uuid,可选自定义marker.options中任意值
+ */
+canvas.deleteMarker({value, name});//删除指定标注点
+```
+
+## 4. `options` 参数
+
+### 必填参数
+
+- **imageUrl**: `string`  
+  指定标注所用的图片地址。
+
+
+- **markers**: `array`  
+  包含一个或多个标记点的经纬度信息。
+
+### 选填参数
+- **imageLoadWay**: `string`
+  图片加载的方式，默认为 "none"，可选值：
+    - **none**: 图片居中显示，大小超出canvas大小时，等比例缩小，直到图片能完全显示
+    - **fill**: 图片平铺canvas画框
+
+- **markerType**: `string`  
+  标注的类型，默认为 "circle"，可选值：
+    - **circle**: 圆形标记点
+    - **triangle**: 圆形标记点
+    - **square**: 正方形标记点
+
+
+- **markerColor**: `string`  
+  标注的颜色。
+
+
+- **markerRadius**: `number`  
+  标注的半径大小。
+
+
+- **markerLineWidth**: `number`  
+  标注边框的线宽。
+
+
+- **hasText**: `boolean`  
+  标注是否包含文字，默认为 `false`。
+
+
+- **textDirection**: `string`  
+  文字的方向，默认为 "right"。 
+
+
+- **autoScaleMarker**: `boolean`  
+  标记是否跟随图片缩放，默认为 `false`。 
+
+
+- **clickMethod**: `function`  
+  点击标注时触发的额外事件处理方法或函数，用以设置标注信息。
+
+### 示例用法
+```js
+function handleMarkerClick(marker) {
+    marker.name = 'Marker Name';
+    marker.uuid = '1234567890';
+    marker.option = {}
+}
+let markers = [];
+let imageUrl = '';
+const options = {
+    imageUrl: imageUrl,
+    markers: markers,
+    clickMethod: handleMarkerClick
+    }
+```
     
